e9f40900b474b248cca533ef10a538a706e01913
Frank Zalkow <frank.zalkow@audiolabs-erlangen.de>
2021-07-15 13:49:30 +0200

    version 1.2.0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md', 'r') as fdesc:
 
 setup(
     name='libfmp',
-    version='1.1.2',
+    version='1.2.0',
     description='Python module for fundamentals of music processing',
     author='Meinard Müller and Frank Zalkow',
     author_email='meinard.mueller@audiolabs-erlangen.de',
